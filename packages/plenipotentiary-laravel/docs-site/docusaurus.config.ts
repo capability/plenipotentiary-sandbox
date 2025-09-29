8f622a0bad5f10bdd887aed5cf7ca6943c7bed56
Sam Davey <samjwdavey@gmail.com>
2025-09-29 15:50:42 +0100

    chore: changed Docusaurus url to https://pleni.dev

--- packages/plenipotentiary-laravel/docs-site/docusaurus.config.ts
+++ packages/plenipotentiary-laravel/docs-site/docusaurus.config.ts
@@ -7,8 +7,8 @@ const config = {
   title: "Plenipotentiary",
   tagline:
     "A Laravel-first orchestration and anti-corruption layer for large APIs - not a wrapper!",
-  url: "https://capability.github.io", // Just the domain
-  baseUrl: "/plenipotentiary-sandbox/", // The sub-path for GitHub Pages
+  url: "https://pleni.dev", // Just the domain
+  baseUrl: "/", // The sub-path for GitHub Pages
   favicon: "img/favicon.ico",
   organizationName: "capability", // GitHub organization
   projectName: "plenipotentiary-sandbox", // repo name
